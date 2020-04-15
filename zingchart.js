21f8b20fc645b2b4d820d3a1af6b91797b6b03bf
Lpwestlake <57381750+Lpwestlake@users.noreply.github.com>
2020-04-14 22:08:05 -0700

    updates to JS and Jupyter notebook

--- zingchart.js
+++ zingchart.js
@@ -2,30 +2,42 @@
 // https://www.zingchart.com/docs/api/json-configuration/
 // Chart Data
 
-d3.csv("chicago_data.csv", function (data) {
+d3.csv("final_chicago_data.csv", function (data) {
 
   data.forEach(function (parse) {
-    // parse.day = +parse.day;
-    // parse.month = +parse.month;
+    parse.primary_type = +parse.primary_type;
+    parse.month = +parse.month;
     // parse.year = +parse.year;
     parse.latitude = +parse.latitude
     parse.longitude = +parse.longitude
   });
 
   console.log(data[1]);
-  var domestic = []
-  var non_domestic = []
+  var domestic_20 = []
+  var non_domestic_20 = []
+  var domestic_19 = []
+  var non_domestic_19 = []
+
   for (var i = 0; i < data.length; i++) {
-    if (data[i].domestic === "True") {
-      domestic.push(data[i].date)
+    if (data[i].domestic === "True" && data[i].year === '2020') {
+      domestic_20.push(data[i].primary_type)
+    }
+    else if (data[i].domestic === "False" && data[i].year === '2020'){
+      non_domestic_20.push(data[i].primary_type)
+    }
+    else if (data[i].domestic === "True" && data[i].year === '2019'){
+      domestic_19.push(data[i].primary_type)
     }
     else {
-      non_domestic.push(data[i].date)
+      non_domestic_19.push(data[i].primary_type)
     }
   }
 
-  console.log(domestic)
-  console.log(non_domestic)
+  console.log(domestic_20)
+  console.log(non_domestic_20)
+  console.log(domestic_19)
+  console.log(non_domestic_19)
+
 
 
   // let domestic = [3, 3, 4, 6, 4, 3, 3, 5, 6, 7, 9, 10];
@@ -67,7 +79,7 @@ d3.csv("chicago_data.csv", function (data) {
     },
     tooltip: {
       callout: true,
-      text: '%t:<br>%v Requests received between<br>Jan 2019 & April 2020',
+      text: '%t:<br>%v Requests received between<br>Jan 2020 & April 2020',
       backgroundColor: '#F7F9FA',
       fontColor: '#505050',
       fontSize: '18px',
@@ -91,16 +103,16 @@ d3.csv("chicago_data.csv", function (data) {
       ]
     },
     scaleY: {
-      values: '0:100:2'
+      values: '0:17500:250'
     },
     series: [
       {
-        values: domestic,
-        text: 'Domestic',
-        zIndex: 8,
+        values: domestic_20,
+        text: 'Domestic 2020',
+        zIndex: 3,
         alphaArea: 1,
         lineColor: '#ffffff',
-        lineWidth: '3px',
+        lineWidth: '1.5px',
         backgroundColor: '#62E9D8',
         marker: {
           size: '0px'
@@ -112,9 +124,9 @@ d3.csv("chicago_data.csv", function (data) {
         }
       },
       {
-        values: non_domestic,
-        text: 'Non-Domestic',
-        zIndex: 9,
+        values: non_domestic_20,
+        text: 'Non-Domestic 2020',
+        zIndex: 1,
         alphaArea: 1,
         lineColor: '#ffffff',
         backgroundColor: '#70DE99',
@@ -126,40 +138,40 @@ d3.csv("chicago_data.csv", function (data) {
           backgroundColor: '#fff',
           borderColor: '#eee'
         }
+      },
+    {
+      values: domestic_19,
+      text: 'Domestic 2019',
+      zIndex: 2,
+      alphaArea: 1,
+      lineColor: '#ffffff',
+      backgroundColor: '#B2DC93',
+      marker: {
+        size: '0px'
+      },
+      hoverMarker: {
+        size: '10px',
+        backgroundColor: '#fff',
+        borderColor: '#eee'
       }
-    ]
-    // {
-    //   values: subpoenas,
-    //   text: 'Subpoenas',
-    //   zIndex: 8,
-    //   alphaArea: 1,
-    //   lineColor: '#ffffff',
-    //   backgroundColor: '#B2DC93',
-    //   marker: {
-    //     size: '0px'
-    //   },
-    //   hoverMarker: {
-    //     size: '10px',
-    //     backgroundColor: '#fff',
-    //     borderColor: '#eee'
-    //   }
-    // },
-    // {
-    //   values: usRequests,
-    //   text: 'US Requests',
-    //   zIndex: 7,
-    //   alphaArea: 1,
-    //   lineColor: '#ffffff',
-    //   backgroundColor: '#FFE295',
-    //   marker: {
-    //     size: '0px'
-    //   },
-    //   hoverMarker: {
-    //     size: '10px',
-    //     backgroundColor: '#fff',
-    //     borderColor: '#eee'
-    //   }
-    // },
+    },
+    {
+      values: non_domestic_19,
+      text: 'Non-Domestic 2019',
+      zIndex: 0,
+      alphaArea: 1,
+      lineColor: '#ffffff',
+      backgroundColor: '#FFE295',
+      marker: {
+        size: '0px'
+      },
+      hoverMarker: {
+        size: '10px',
+        backgroundColor: '#fff',
+        borderColor: '#eee'
+      }
+    }
+  ]
     // {
     //   values: governmentRequests,
     //   text: 'Government Requests',
