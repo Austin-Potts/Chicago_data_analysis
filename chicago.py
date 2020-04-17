cafde657cda7b251dff3b7d1dab27f12420e9994
unknown <austinpotts01@gmail.com>
2020-04-16 19:52:14 -0500

    fixed chicago.py

--- chicago.py
+++ chicago.py
@@ -27,17 +27,19 @@ def getData():
     )
     client.close()
 
+    #reformatting the data from the api call and organizing the results
     df['month'] = pd.DatetimeIndex(df['date']).month
     df['year'] = pd.DatetimeIndex(df['date']).year
     df['date'] = pd.to_datetime(df['date']).dt.strftime('%m.%d.%Y')
 
+    #converting strings from all caps to an easier to read format
     df["primary_type"] = df["primary_type"].str.lower().str.title()
     df["description"] = df["description"].str.lower().str.title()
     df["location"] = df["location_description"].str.lower().str.title()
 
-    # Organize: 
-    current_df = df[["date","month", "year","primary_type", "description","latitude", "longitude", "domestic"]]
-    current_df.shape
+    # Organize columns: 
+    df2020 = df[["date","month", "year","primary_type", "description","latitude", "longitude", "domestic"]]
+    df2020.shape
 
     df1 = pd.DataFrame(
         client.get(
@@ -49,25 +51,51 @@ def getData():
     )
     client.close()
 
+    #reformatting the data from the api call and organizing the results
     df1['month'] = pd.DatetimeIndex(df1['date']).month
     df1['year'] = pd.DatetimeIndex(df1['date']).year
     df1['date'] = pd.to_datetime(df1['date']).dt.strftime('%m.%d.%Y')
 
+    #converting strings from all caps to an easier to read format
     df1["primary_type"] = df1["primary_type"].str.lower().str.title()
     df1["description"] = df1["description"].str.lower().str.title()
     df1["location"] = df1["location_description"].str.lower().str.title()
 
-    # Organize: 
-    old_df = df1[["date","month","year","primary_type","description","latitude","longitude","domestic"]]
-    old_df.shape
+    # Organize columns: 
+    df2019 = df1[["date","month","year","primary_type","description","latitude","longitude","domestic"]]
+    df2019.shape
 
-    dataframes = [old_df, current_df]
+    #second api call to recieve the data from 2019
+    df2 = pd.DataFrame(
+        client.get(
+            crime_data, 
+            where=f"Date BETWEEN '2018-01-01' AND '2018-{month}-{day}'",
+            limit=100000,
+            exclude_system_fields=True
+        )
+    )
+    client.close()
 
+    #reformatting the data from the api call and organizing the results
+    df2['month'] = pd.DatetimeIndex(df2['date']).month
+    df2['year'] = pd.DatetimeIndex(df2['date']).year
+    df2['date'] = pd.to_datetime(df2['date']).dt.strftime('%m.%d.%Y')
+
+    #converting strings from all caps to an easier to read format
+    df2["primary_type"] = df2["primary_type"].str.lower().str.title()
+    df2["description"] = df2["description"].str.lower().str.title()
+    df2["location"] = df2["location_description"].str.lower().str.title()
+
+    # Organize columns: 
+    df2018 = df2[["date","month","year","primary_type","description","latitude","longitude","domestic"]]
+    df2018.shape
+
+    #combining the 2019 and 2020 dataframes into one, also resetting the index
+    dataframes = [df2018, df2019, df2020]
     final_df = pd.concat(dataframes)
-
     final_df.reset_index(inplace=True)
-
-    print(f"{final_df.shape}")
+    print(final_df.shape)
+    final_df.head()
 
 
     # dom_df = final_df.filter(like = 'True')
